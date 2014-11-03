6972c6d01ac5b3c1380fd9f061e38465f752c661
Kemel Zaidan <kemelzaidan@gmail.com>
2014-11-03 19:41:01 -0200

    adding logic to the stream

--- bot.rb
+++ bot.rb
@@ -21,9 +21,37 @@ end
 
 # user stream connection
 @client  = TweetStream::Client.new
- 
+
 puts "[STARTING] bot..."
 @client.userstream() do |status| 
-  
-  puts status.text  # print do texto do tweet
+
+# check if stream is not a retwit or indication
+if !status.retweet? &&
+   status.in_reply_to_user_id? && status.in_reply_to_user_id == ACCOUNT_ID &&
+   status.text[-1] == "?"
+ 
+     tweet = {
+       "status" => "@#{status.user.screen_name} " + %w(Sim Não Talvez).sample,
+       "in_reply_to_status_id" => status.id.to_s
+     }
+ 
+     twurl = URI.parse("https://api.twitter.com/1.1/statuses/update.json")
+     authorization = SimpleOAuth::Header.new(:post, twurl.to_s, tweet, OAUTH)
+ 
+     http = EventMachine::HttpRequest.new(twurl.to_s).post({
+       :head => {"Authorization" => authorization},
+       :body => tweet
+     })
+     http.errback {
+       puts "[CONN_ERROR] errback"
+     }
+     http.callback {
+       if http.response_header.status.to_i == 200
+         puts "[HTTP_OK] #{http.response_header.status}"
+       else
+         puts "[HTTP_ERROR] #{http.response_header.status}"
+       end
+     }
+ 
+ end
 end

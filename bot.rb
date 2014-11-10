d93fb00449e0feb0f8bd6cd44473b775263c4638
Kemel Zaidan <kemelzaidan@gmail.com>
2014-11-10 17:45:25 -0200

    working version :-)

--- bot.rb
+++ bot.rb
@@ -26,25 +26,6 @@ end
 COTATIONS = {}
 BIT_AVERAGE_URL = "https://api.bitcoinaverage.com/all" #bitcoinaverage.com API endpoint
 
-def final_amount(amount, currency)
-  COTATIONS[:data][currency]["averages"]["last"] * amount
-end
-
-def update_cotations
-  return if cotations_updated?
-  
-  puts "Will fetch new cotations"
-  response = HTTParty.get(BIT_AVERAGE_URL)
-  COTATIONS[:data] = JSON.parse response.body
-  COTATIONS[:timestamp] = Time.now
-  puts "Cotations fetched"
-  return COTATIONS
-end
-
-def cotations_updated?
-  COTATIONS[:timestamp] && (Time.now - COTATIONS[:timestamp]) < 10
-end
-
 #def fetch_cotations
 #  response = HTTParty.get(BIT_AVERAGE_URL)
 #  COTATIONS[:data] = JSON.parse response.body
@@ -81,32 +62,48 @@ puts "[STARTING] bot..."
         puts "[PROCESSING] #{status.text}"
         bit_amount = status.text[bit_regex].to_f # grabs the amount on the tweet
         currency = status.text[currency_regex][1..-1] # takes the "#" out
-        #p bit_amount
-        #p currency
+        p bit_amount
+        p currency
 
         # bloquing cotation update
        	operation = proc {
-        	update_cotations              
+        	def cotations_updated?
+              COTATIONS[:timestamp] && (Time.now - COTATIONS[:timestamp]) < 10
+            end
+          
+          
+            if !cotations_updated?
+                puts "Will fetch new cotations"
+                response = HTTParty.get(BIT_AVERAGE_URL)
+                COTATIONS[:data] = JSON.parse response.body
+                COTATIONS[:timestamp] = Time.now
+                puts "Cotations fetched"
+                #return COTATIONS
+            end
+
+            def final_amount(amount, currency)
+                puts "Will compute final_amount"
+            	COTATIONS[:data][currency]["averages"]["last"] * amount
+        	end
+
+            result = final_amount(bit_amount, currency)
+            puts "Should return #{result}"
+        	result.round(2)
         }
 
-        callback = proc {
-        	this_amount = final_amount(bit_amount, currency)              
-        }
-				
-        EventMachine.defer(operation, callback)
-        
+            callback = proc { |this_amount|
+                
         #create the reply tweet
-        @reply = "#{bit_amount} bitcoins in #{currency} is #{this_amount}"
+        reply = "#{bit_amount} bitcoins in #{currency} is #{this_amount}"
         puts reply
         
-        puts "got out of the defer"
         tweet = {
             "status" => "@#{status.user.screen_name} " + reply,
             "in_reply_to_status_id" => status.id.to_s
             }
         puts tweet
         
-				authorization = SimpleOAuth::Header.new(:post, twurl.to_s, tweet, OAUTH)
+		authorization = SimpleOAuth::Header.new(:post, twurl.to_s, tweet, OAUTH)
         
         http = EventMachine::HttpRequest.new(twurl.to_s).post({
             	:head => {"Authorization" => authorization},
@@ -122,30 +119,8 @@ puts "[STARTING] bot..."
                  puts "[HTTP_ERROR] #{http.response_header.status}"
                end
           }
-
+        }
         
-#        #if has_cotation == false || (cotation_timestamp - Time.now) > 10 # if there was elapsed
-#        EventMachine.run {
-#            conversion_url = bit_average_url + currency + "/"
-#            puts conversion_url
-#            # gets the bitcoin cotation at bitcoinaverage.com API
-#            get_cotation = EventMachine::HttpRequest.new(conversion_url).get
-# 
-#            get_cotation.errback { puts "[ERROR] errback" }
-#            get_cotation.callback {
-#                json = JSON.parse get_cotation.response
-#                bitcoin_cotation = json["last"]
-#                has_cotation = true
-#                cotation_timestamp = json["timestamp"]
-#                final_amount = bitcoin_cotation * bit_amount
-# 
-#                http.
-#                
-#                EventMachine.stop
-#            }
-#        }
-#        puts "estou fora da EventMachine"
-#    else
-#        puts "Nao deveria passar por aqui"
+        EventMachine.defer(operation, callback)
     end
 end
